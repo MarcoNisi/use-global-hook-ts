42b71a9455bb6f90082b6092304f34765817d70b
Marco Nisi <marco.nisi.100@gmail.com>
2019-08-23 11:51:28 +0200

    Splitted in modules & improved typings

---
+++ interfaces.ts
@@ -0,0 +1,27 @@
+export interface IStore<S> {
+  state: S
+  setState: (changes: DeepPartial<S>) => void
+  listeners: any[]
+  actions: any[]
+  debug: boolean
+}
+
+export type DeepBoolPartial<T> = {
+  [P in keyof T]?: T[P] extends Array<infer U>
+    ? Array<DeepBoolPartial<U>> | boolean
+    : T[P] extends ReadonlyArray<infer U>
+    ? ReadonlyArray<DeepBoolPartial<U>> | boolean
+    : DeepBoolPartial<T[P]> extends object
+    ? DeepBoolPartial<T[P]> | boolean
+    : boolean
+}
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends Array<infer U>
+    ? Array<DeepPartial<U>>
+    : T[P] extends ReadonlyArray<infer U>
+    ? ReadonlyArray<DeepPartial<U>>
+    : DeepPartial<T[P]> extends object
+    ? DeepPartial<T[P]>
+    : T[P]
+}
---
+++ utils.ts
@@ -0,0 +1,53 @@
+export const deepUpdate = (oldObject: any, changes: any) => {
+  for (const prop in changes) {
+    try {
+      if (changes[prop].constructor === Object) {
+        oldObject[prop] = deepUpdate(oldObject[prop], changes[prop])
+      } else if (oldObject.hasOwnProperty(prop)) {
+        oldObject[prop] = changes[prop]
+      }
+    } catch (e) {
+      oldObject[prop] = changes[prop]
+    }
+  }
+  return oldObject
+}
+
+export const overlap = (primary: any, secondary: any): any => {
+  if (!secondary) return primary
+  return Object.assign({}, ...Object.keys(primary).map((k: string) => {
+    if (!(k in secondary)) {
+      return {}
+    }
+    if (
+      primary[k] &&
+      typeof primary[k] === 'object' &&
+      secondary[k] &&
+      typeof secondary[k] === 'object'
+    ) {
+      let tmp = overlap(primary[k], secondary[k])
+      return Object.keys(tmp).length ? { [k]: tmp } : {}
+    }
+    if (secondary[k] === true) {
+      return { [k]: primary[k] }
+    }
+    return {}
+  }))
+}
+
+export const shouldUpdate = (listenedTree: any, changes: any): boolean => {
+  const commonKeys = overlap(changes, listenedTree)
+  return Object.keys(commonKeys).length > 0
+}
+
+export const cloneDeep = (oldObject: any) => {
+  let newObject: any = Array.isArray(oldObject) ? [] : {}
+  for (const prop in oldObject) {
+    if (typeof oldObject[prop] === 'object') {
+      newObject[prop] = cloneDeep(oldObject[prop])
+    } else {
+      newObject[prop] = oldObject[prop]
+    }
+  }
+  return newObject
+}
